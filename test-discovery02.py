db0149282ca0f5cab6c8d80da4b25f70152f28bc
lubusax <lu.bu.sax@gmail.com>
2020-06-06 23:02:26 +0200

    autosave

--- test-discovery02.py
+++ test-discovery02.py
@@ -29,6 +29,8 @@ def print_normal(properties):
 
 def interfaces_added(path, interfaces):
 	properties = interfaces["org.bluez.Device1"]
+	print(path)
+	print(properties)
 	if not properties:
 		return
 
@@ -36,43 +38,18 @@ def interfaces_added(path, interfaces):
 		print("Name",properties["Name"])
 		if properties["Name"]==nameThingsGate:
 			print("we found it!")
+			path="/org/bluez/hci0/dev_B8_27_EB_E7_44_E3"
+			ifaceWanted = dbus.Interface(bus.get_object("org.bluez", path),
+                                     "org.bluez.Device1")
+			ifaceWanted.Connect()
+			#properties.Connect()
 
 	print_normal(properties)
 
 def properties_changed(interface, changed, invalidated, path):
 	print('properties changed '+'#'*120+ '\n')
 
-	# if interface != "org.bluez.Device1":
-	# 	return
 
-	# if path in devices:
-	# 	dev = devices[path]
-	# 	if compact and skip_dev(dev, changed):
-	# 		return
-	# 	print ('devices path ', pformat(devices[path]), '§'*90)
-	# 	print('devices path items : ', pformat(devices[path].items()), '§'*90)
-	# 	print('changed items ', pformat(changed.items()), '§'*90, '\n')
-	# 	devices[path] = devices[path].update(changed)
-	# 	#print('devices path items AFTER: ', pformat(devices[path], '§'*90)
-	# else:
-	# 	devices[path] = changed
-
-	# #print ('devices path ', devices[path])
-
-	
-
-	# if devices[path] != None : 
-	# 	if ("Address" in devices[path]):
-	# 		address = devices[path]["Address"]
-	# 	else:
-	# 		address = "<unknown>"
-
-	# 	if compact:
-	# 		print_compact(address, devices[path])
-	# 	else:
-	# 		print_normal(address, devices[path])
-	# else:
-	# 	print ('devices[path] is None')
 
 if __name__ == '__main__':
 	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
@@ -80,16 +57,20 @@ if __name__ == '__main__':
 	bus = dbus.SystemBus()
 
 	adapter = bluezutils.find_adapter()
+	path="/org/bluez/hci0/dev_B8_27_EB_E7_44_E3"
+	ifaceWanted = dbus.Interface(bus.get_object("org.bluez", path),
+																"org.bluez.Device1")
+	ifaceWanted.Disconnect()
 
 	bus.add_signal_receiver(interfaces_added,
 			dbus_interface = "org.freedesktop.DBus.ObjectManager",
 			signal_name = "InterfacesAdded")
 
-	bus.add_signal_receiver(properties_changed,
-			dbus_interface = "org.freedesktop.DBus.Properties",
-			signal_name = "PropertiesChanged",
-			arg0 = "org.bluez.Device1",
-			path_keyword = "path")
+	# bus.add_signal_receiver(properties_changed,
+	# 		dbus_interface = "org.freedesktop.DBus.Properties",
+	# 		signal_name = "PropertiesChanged",
+	# 		arg0 = "org.bluez.Device1",
+	# 		path_keyword = "path")
 
 	om = dbus.Interface(bus.get_object("org.bluez", "/"),
 				"org.freedesktop.DBus.ObjectManager")
